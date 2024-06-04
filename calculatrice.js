5d1da85aa9a80fdfcf3f58f6431e963a3acb06c5
Mosheh <moiseraharifetra@gmail.com>
2024-06-04 13:08:55 +0200

    Il y a une changement dans le html et js

--- calculatrice.js
+++ calculatrice.js
@@ -1,5 +1,6 @@
 var result = document.getElementById('resultat');
 var supp = document.getElementById('supprimer');
+var del =  document.getElementById('del');
 var nine = document.getElementById('9');
 var eight = document.getElementById('8');
 var seven = document.getElementById('7');
@@ -10,6 +11,10 @@ var three = document.getElementById('3');
 var two = document.getElementById('2');
 var one = document.getElementById('1');
 var zero = document.getElementById('0');
+var accolade1 = document.getElementById('accolade1');
+var accolade2 = document.getElementById('accolade2');
+var zero2 = document.getElementById('00');
+var racineCarre = document.getElementById('sqrt');
 var virgule = document.getElementById('virgule');
 var equal = document.getElementById('equal');
 var div = document.getElementById('division');
@@ -44,6 +49,9 @@ three.addEventListener("click", () => ajouterValeur(three.value));
 two.addEventListener("click", () => ajouterValeur(two.value));
 one.addEventListener("click", () => ajouterValeur(one.value));
 zero.addEventListener("click", () => ajouterValeur(zero.value));
+zero2.addEventListener("click", () => ajouterValeur(zero2.value));
+accolade1.addEventListener("click", () => ajouterValeur(`(`));
+accolade2.addEventListener("click", () => ajouterValeur(`)`));
 virgule.addEventListener("click", () => ajouterValeur('.'));
 div.addEventListener("click", () => ajouterValeur('/'));
 multi.addEventListener("click", () => ajouterValeur('*'));
@@ -55,10 +63,13 @@ equal.addEventListener("click", () => {
         result.value = eval(result.value);
         calculEffectue = true; // Marque qu'un calcul a été effectué
     } catch (e) {
-        result.value = "Erreur";
+        result.value = "Error";
     }
 });
 
-supp.addEventListener('click', () => {
+del.addEventListener('click', () => {
     result.value = result.value.slice(0, -1);
+});
+supp.addEventListener('click', () => {
+    result.value = result.value.slice(0, -500);
 });
